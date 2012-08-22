cc0cd057894041dd45bc79d4504e460bac606500
felipeandradebr <andrade.felipecoelho@gmail.com>
2012-08-22 00:16:51 -0300

    mudanca no show da view ordem

--- app/models/ordem.rb
+++ app/models/ordem.rb
@@ -4,5 +4,4 @@ class Ordem < ActiveRecord::Base
   has_many :exemplares
   attr_accessible :clase_id, :descricao, :status
   validates_presence_of :descricao, :message =>" - Deve ser preenchido"
-  validates_length_of   :descricao, :minimum => 6,  :message =>" - Deve conter pelo menos 6 letras"
 end
